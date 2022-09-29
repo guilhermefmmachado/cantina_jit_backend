4e10068f20b25f1fcd9748776cba6692d3ed4558
guilhermefmmachado <guilherme.fmachado@outlook.com>
2022-09-29 09:16:46 -0300

    Processo adicionar produto JÁ EXECUTÁVEL

--- classes/produto.php
+++ classes/produto.php
@@ -1,26 +1,83 @@
 <?php
 
 class Produto
+{
+  // ! ATENÇÃO AOS TIPOS DE DADOS, NÃO ENVIE AO BANCO DE DADOS AINDA
+  public $nomeProduto = "";
+  public $tipoProduto = "";
+  public $descricao = "";
+  public $preco = 0;
+  public $qtdeProdVendidos = 0;
+  public $estoque = 0;
+  public $condicaoExecucao = false;
+
+  public $qtdeSelecionadaCliente = 0;
+  public $precoTotal = 0;
+
+  function __construct(String $nome, String $descricao, String $tipo, String $preco, String $estoque, bool $condicaoExecucao)
   {
-    public $nomeProduto = "";
-    public $tipoProduto = "";
-    public $descricao = "";
-    public $preco = 0;
-
-    public function editarProduto()
-    {
-      # code...
-    }
-
-    public function inserirProduto()
-    {
-      # code...
-    }
-
-    public function excluirProduto()
-    {
-      # code...
-    }
+    $this->nomeProduto = $nome;
+    $this->descricao = $descricao;
+    $this->tipoProduto = $tipo;
+    $this->preco = (float) $preco;
+    $this->estoque = (int) $estoque;
+    $this->condicaoExecucao = $condicaoExecucao;
   }
 
-?>
+  public function editarProduto()
+  {
+    # code...
+  }
+
+  public function inserirProduto()
+  {
+    $servername = "localhost";
+    $dbUser = "root";
+    $dbPassword = "root";
+    $db = "cantina_jit_01";
+    // $dbPord = 3306;
+
+    // Realizar conexão com a base de dados
+    $conexao = new mysqli($servername, $dbUser, $dbPassword, $db);
+
+    // Checar a conexão
+    if ($conexao->connect_error) {
+      die("Falha na conexão: " + $conexao->connect_error);
+    }
+
+    $valorRetorno["erro"] = false;
+    $valorRetorno["msg"] = "";
+    $valorRetorno["sucesso"] = false;
+
+    if ($this->condicaoExecucao) {
+      $instrucaoSQL = "INSERT INTO produto SET nome_produto = '$this->nomeProduto',
+      tipo_produto = '$this->tipoProduto', descricao_produto = '$this->descricao',
+      preco = '$this->preco',
+      qtde_produtos_vedidos = '$this->qtdeProdVendidos',
+      estoque = '$this->estoque'";
+
+      $resultado = $conexao->query($instrucaoSQL);
+
+      if (!$resultado) {
+        // echo "Erro na conexão $this->nomeProduto,\n $this->tipoProduto,\n $this->descricao,\n $this->preco,\n $this->estoque,\n $resultado,\n $instrucaoSQL";
+        $valorRetorno["erro"] = false;
+        $valorRetorno["msg"] = "Erro no banco de dados";
+      }
+    } else {
+      $return["error"] = true;
+      $return["message"] = 'Informe o formulário corretamente.';
+    }
+
+    $conexao->close();
+
+    // Informando à plataforma que ocorrerá um retorno JSON
+    header("Content-Type: application/json");
+    // Transformando o array em uma string json;
+    echo json_encode($valorRetorno);
+  }
+
+  public function excluirProduto()
+  {
+    # code...
+  }
+}
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+
+  require_once "classes/produto.php";
   
+  $condicaoExecucao = isset($_POST["nome"]) && isset($_POST["tipo"]) && isset($_POST["preco"]) && isset($_POST["estoque"]);
+
+  $novoProduto = new Produto($_POST["nome"], $_POST["desc"], $_POST["tipo"], $_POST["preco"], $_POST["estoque"], $condicaoExecucao);
+
+  echo $novoProduto->inserirProduto();
 ?>
